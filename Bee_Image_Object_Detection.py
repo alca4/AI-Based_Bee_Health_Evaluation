1cca2a1ba7088b3db5481a4cad1447bf7dd004d2
alca4 <57308642+alca4@users.noreply.github.com>
2024-10-05 10:39:27 -0700

    Update Bee_Image_Object_Detection.py
    Add comments

--- Bee_Image_Object_Detection.py
+++ Bee_Image_Object_Detection.py
@@ -1,17 +1,16 @@
-# Commented out IPython magic to ensure Python compatibility.
+# Clone YOLOv5 repository
 !git clone https://github.com/ultralytics/yolov5
-# %cd yolov5
 
-# Commented out IPython magic to ensure Python compatibility.
-# %pip install -qr requirements.txt
+# Navigate to the YOLOv5 directory
+%cd yolov5
 
-# Commented out IPython magic to ensure Python compatibility.
+# Install necessary dependencies
+!pip install -r requirements.txt
 
 from yolov5 import utils #go to folder Bee_Object_detection
 import torch
 
 import utils
-# import core
 from IPython import display
 from IPython.display import clear_output
 from pathlib import Path
@@ -20,16 +19,26 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import glob
 
-
-# %matplotlib inline
+# Initialize utilities for better notebook interaction
 display = utils.notebook_init()
 
+# View the Bee dataset configuration file
 !cat data/Bee.yaml
 print ('---------------------------------------------')
 
+# Train YOLOv5 model
+# Using `yolov5m6.pt` as a starting point with 50 epochs and freezing the first 12 layers
 !python train.py --batch 64 --epochs 50 --data 'data/Bee.yaml' --weights 'yolov5m6.pt' --project 'run_bee' --name 'feature_extraction' --cache --freeze 12
-# clear_output()
 
+# Clear output
+clear_output()
+
+# Display training results
 display.Image(f"run_bee/feature_extraction9/results.png")
 
+# Test detection with the trained model
+# Change 'run_bee/feature_extraction7/weights/best.pt' to the correct path to the best model from the training phase
 !python detect.py --weights 'run_bee/feature_extraction7/weights/best.pt'  --conf 0.6 --source './Downloads/Bee_Object_Detection/yolov5/test/images' --project 'run_bee' --name 'detect_test' --augment --line=3 --save-txt --save-crop
+
+# View detection results
+display.Image(f"run_bee/detect_test/exp/image0.jpg")
